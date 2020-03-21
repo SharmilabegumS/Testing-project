20ac6df2ec431266ee4472e90295f65687dca215
sharmila-9182 <sharmila.b@zohocorp.com>
2020-03-21 21:41:42 +0530

    linked list

--- Testing project/Data Structure/main.swift
+++ Testing project/Data Structure/main.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+
 print("------Array Excercise------")
 var objArray = ArrayExcercise()
 objArray.doAction()
@@ -96,5 +97,15 @@ print("Removed value: " + String(describing: removedValue1))
 
 
 
-
+print("using collection")
+var list = LinkedList<Int>()
+for i in 0...9 {
+    list.append(i)
+}
+print("List: \(list)")
+print("First element: \(list[list.startIndex])")
+print("Array containing first 3 elements: \(Array(list.prefix(3)))")
+print("Array containing last 3 elements: \(Array(list.suffix(3)))")
+let sum = list.reduce(0, +)
+print("Sum of all values: \(sum)")
 
